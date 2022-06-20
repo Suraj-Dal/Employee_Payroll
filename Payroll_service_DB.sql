82a20c11dee7ee2791bc3259a934cb854f03f886
Suraj-Dal <dsurajdal10@gmail.com>
2022-06-20 11:41:26 +0530

    [Suraj]: Usecase 2 to create table named employee payroll is added.

--- Payroll_service_DB.sql
+++ Payroll_service_DB.sql
@@ -3,3 +3,17 @@ create database Payroll_Service_DB;
 --user Database
 use Payroll_Service_DB;
 
+--Create table
+CREATE TABLE employee_payroll 
+(
+   id int identity primary key,
+   name varchar(Max) Not null,
+   salary money default 1000,
+   startDate DateTime default GetDate()
+);
+
+select * from employee_payroll;
+
+
+
+
